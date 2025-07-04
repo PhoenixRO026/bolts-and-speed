9cbe0a5337b3561ed6f344a4a0bf744183a39610
Emi <emi.serban@gmail.com>
2025-07-04 16:17:06 +0300

    added ftc dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.kt
@@ -1,13 +1,17 @@
-package org.firstinspires.ftc.teamcode.teleop.tests
+package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 
 @TeleOp
 class LiftTest : LinearOpMode() {
     override fun runOpMode() {
+        telemetry = MultipleTelemetry(telemetry, FtcDashboard.getInstance().telemetry)
+
         val liftMotorRight = hardwareMap.get(DcMotor:: class.java, "motorR")
         val liftMotorLeft = hardwareMap.get(DcMotor:: class.java, "motorL")
 
@@ -21,6 +25,9 @@ class LiftTest : LinearOpMode() {
 
             liftMotorLeft.power = speed
             liftMotorRight.power = speed
+
+            telemetry.addData("speed", speed)
+            telemetry.update()
         }
     }
 }
