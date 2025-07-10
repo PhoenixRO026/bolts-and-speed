c97bbed30c5fe74108b54e05c45275dfcdcbe949
ProNoob1029 <pronoob1029@gmail.com>
2025-07-10 21:37:35 +0300

    steer fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.kt
@@ -14,17 +14,31 @@ class TeleOp : LinearOpMode(){
         val motorLeft = hardwareMap.get(DcMotor::class.java, "motorL")
         val servo = hardwareMap.get(Servo::class.java, "servo")
 
+        val servoMin = 0.0
+        val servoMax = 1.0
+        var servoPos: Double
+        var input: Double
+
         motorLeft.direction = DcMotorSimple.Direction.REVERSE
 
         waitForStart()
 
         while(opModeIsActive()) {
-            val power = -gamepad1.left_stick_y.toDouble()
+            val back = gamepad1.left_trigger.toDouble()
+            val forward = gamepad1.right_trigger.toDouble()
+            val power = forward - back
             motorRight.power = power
             motorLeft.power = power
 
-            val direction = gamepad1.right_stick_x.toDouble()
-            servo.position = direction * direction * direction.sign
+            val direction = gamepad1.left_stick_x.toDouble()
+            input = direction * direction * direction.sign
+            servoPos = input * 0.5 + 0.5
+            servoPos = servoPos * (servoMax - servoMin) + servoMin
+            servo.position = servoPos
+
+            telemetry.addData("pos", servoPos)
+            telemetry.addData("input", input)
+            telemetry.update()
         }
     }
 }
